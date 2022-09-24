7dabfd550d9612adebef674253a806931d59c218
Astar18 <aramirez@itsqmet.edu.ec>
2022-09-24 00:43:45 -0500

    init commit

---
+++ src/main/java/ec/edu/itsqmet/InitSpringApplication.java
@@ -0,0 +1,13 @@
+package ec.edu.itsqmet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InitSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InitSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ec/edu/itsqmet/InitSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package ec.edu.itsqmet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InitSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
