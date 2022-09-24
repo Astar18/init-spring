ec6ffa982293e97f1b35ecd81411d83d1fd30bf4
Astar18 <aramirez@itsqmet.edu.ec>
2022-09-24 02:41:39 -0500

    3

---
+++ src/main/java/ec/edu/itsqmet/service/IClientService.java
@@ -0,0 +1,11 @@
+package ec.edu.itsqmet.service;
+
+public interface IClientService {
+	public String saludar();
+	public String saludP();
+	public String saludPT();
+	public String saludD();
+	public String saludPH();
+}
+	
+	
